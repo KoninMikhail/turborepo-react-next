af6c955f5fd9a12a4126ed841b021f37de07c681
Javi Aguilar <122741+itsjavi@users.noreply.github.com>
2022-11-11 19:54:39 +0100

    integrate styled-components in ui package

--- packages/ui/babel.config.js
+++ packages/ui/babel.config.js
@@ -3,4 +3,12 @@ module.exports = {
     '@babel/preset-env',
     ['@babel/preset-react', { runtime: 'automatic' }],
   ],
+  plugins: [
+    [
+      'babel-plugin-styled-components',
+      {
+        ssr: false,
+      },
+    ],
+  ],
 }
